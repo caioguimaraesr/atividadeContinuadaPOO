1380492810228a607561f9cef2bcda250e96a174
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-09-14 20:24:45 -0300

    Enum - TipoOrdem (Incompleto)

---
+++ src/br/edu/cs/poo/ac/ordem/entidades/TipoOrdem.java
@@ -0,0 +1,23 @@
+package br.edu.cs.poo.ac.ordem.entidades;
+
+public enum TipoOrdem {
+	MANUTENCAO(1, "Manutenção"),
+    CONFIGURACAO(2, "Configuração"),
+    UPGRADE(3, "Upgrade");
+
+	private int codigo;
+	private final String nome; 
+	
+	private TipoOrdem(int codigo, String nome) {
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+	
+	public int getCodigo() {
+		return this.codigo;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}	
+}
